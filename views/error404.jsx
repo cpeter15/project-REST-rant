071fb489788dbe8f055c628972410b507b93b9e0
Caleb Peterson <calebpeterson@calebs-mbp.myfiosgateway.com>
2022-07-21 20:32:25 -0400

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,11 @@ const error404 = () => {
         <main>
             <h1>404: PAGE NOT FOUND</h1>
             <p>Oops, sorry, we can't find this page!</p>
+            <img src="images/404-image.jpg"/>
+            <div>
+            Photo by <a href="https://unsplash.com/es/@theodorrr?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Theodor Vasile</a> on <a href="https://unsplash.com/s/photos/horizion?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+            </div>
         </main>
     )
 }
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,9 +4,15 @@ const Def = require('../default');
 const index = (data) => {
     let placesFormatted = data.places.map((place) => {
         return (
-            <div>
+            <div className="col-sm-6">
                 <h2>{place.name}</h2>
+                <p className="tex-center">
+                    {place.cuisines}
+                </p>
                 <img src={place.pic} alt={place.name}></img>
+                <p className="text-center">
+                    Located in {place.city}, {place.state}
+                </p>
             </div>
         )
     })
@@ -14,7 +20,9 @@ const index = (data) => {
         <Def>
             <main>
                 <h1>PLACES INDEX PAGE</h1>
+                <div className="row">
                 {placesFormatted}
+                </div>
             </main>
         </Def>
     )
