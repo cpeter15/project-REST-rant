e0b1c7f32da9df5aca5d3d733ca37c5bb47da007
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-07-24 11:15:20 -0400

    styled show.jsx

--- controllers/places.js
+++ controllers/places.js
@@ -27,14 +27,14 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-    res.render('places/show');
+    let id = Number(req.params.id);
     if (isNaN(id)) {
         res.render('error404');
     } else if (!places[id]) {
         res.render('error404');
     } else {
 
-        res.render('places/show', { place: places[id] });
+        res.render('places/show', { place: places[id], id });
     }
 })
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,35 @@
 const React = require('react')
 const Def = require('../default')
 
-const show = () => {
+function show(data) {
     return (
         <Def>
             <main>
-                <h1>Show Page</h1>
+                <img src={data.place.pic}></img>
+                <div>
+                    <h1>{data.place.name}</h1>
+                    <div>
+                        <h2>Rating</h2>
+                        <p>Unrated</p>
+                    </div>
+                    <div>
+                        <h2>Description</h2>
+                        <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
+                    </div>
+                    <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                        Edit
+                    </a>
+                    <form method="POST" action={`/place/${data.id}?_method=DELETE`}>
+                        <button type="submit" className="btn btn-danger">
+                            Delete
+                        </button>
+                    </form>
+
+                </div>
+                <div>
+                    <h2>Comments</h2>
+                    <p>comments unavailable</p>
+                </div>
             </main>
         </Def>
     )
