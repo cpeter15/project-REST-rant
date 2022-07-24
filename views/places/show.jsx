24dd0c2d7442b39c4b2398e3116c3922644906f8
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-07-24 11:23:57 -0400

    Delete functionality implemented

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -19,7 +19,7 @@ function show(data) {
                     <a href={`/places/${data.id}/edit`} className="btn btn-warning">
                         Edit
                     </a>
-                    <form method="POST" action={`/place/${data.id}?_method=DELETE`}>
+                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
                         <button type="submit" className="btn btn-danger">
                             Delete
                         </button>
