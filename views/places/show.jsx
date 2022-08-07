e4ab09b75d41d14b8e73e0cca5d70c68ba1a97ca
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-08-07 03:42:43 -0400

    averaged ratings

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,22 @@ function show(data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className='inactive'>
+            Not yet rated
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings / data.place.comments.length
+        rating = (
+            <h3>
+                {Math.round(averageRating)} stars
+            </h3>
+        )
+
         comments = data.place.comments.map(c => {
             return (
                 <div className="border">
@@ -29,8 +44,11 @@ function show(data) {
                 <div>
                     <h1>{data.place.name}</h1>
                     <div>
-                        <h2>Rating</h2>
-                        <p>Unrated</p>
+                        <h2>
+                            Rating
+                        </h2>
+                        {rating}
+                        <br />
                     </div>
                     <div>
                         <h2>Description</h2>
